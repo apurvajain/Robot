815985ff993ec892039f6b0563eb5c6bef2e5f4d
apurvajain <apurvajain2393@gmail.com>
2015-02-05 12:12:47 +0530

    More Specs added for parser class.

--- spec/navigate/parser_spec.rb
+++ spec/navigate/parser_spec.rb
@@ -8,4 +8,40 @@ describe 'Parser' do
   expect(parser1.parsing("1 2 north", "move move" ).class).to eq (Robot)
  end
 
+ it 'check parsing of input' do
+  expect(parser1.parsing("1 2 south", "move left" ).class).to eq (Robot)
+ end
+
+ it 'check parsing of input' do
+  expect(parser1.parsing("1 2 north", "move left move" ).class).to eq (Robot)
+ end
+
+ it 'check parsing of input' do
+  expect(parser1.parsing("1 2 east", "move right" ).class).to eq (Robot)
+ end
+
+ it 'check parsing of input' do
+  expect(parser1.parsing("1 2 north", "left right" ).class).to eq (Robot)
+ end
+
+ it 'check parsing of input' do
+  expect(parser1.parsing("1 2 west", "move move right" ).class).to eq (Robot)
+ end
+ 
+ it 'check parsing of input' do
+  expect(parser1.parsing("1 2 north", "right move" ).class).to eq (Robot)
+ end
+ 
+ it 'check parsing of input' do
+  expect(parser1.parsing("1 2 north", "left move right left" ).class).to eq (Robot)
+ end
+ 
+ it 'check parsing of input' do
+  expect(parser1.parsing("1 3 east", "right move right" ).class).to eq (Robot)
+ end
+
+  it 'check parsing of input' do
+  expect(parser1.parsing("1 2 north", "right move right" ).class).to eq (Robot)
+ end
+
 end
